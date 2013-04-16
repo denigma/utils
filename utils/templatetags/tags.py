9ac1d1d0eb48df3f0bb61858f8a2ea137af86eed
hevok <age@liv.ac.uk>
2013-04-16 21:25:11 +0200

    Made application Python 3 compatible.

--- utils/templatetags/tags.py
+++ utils/templatetags/tags.py
@@ -13,7 +13,8 @@ def active(request, urls):
 
 @register.simple_tag
 def location(request):
-    if request.path == '/': return u' » ' + 'Home'
-    return u' » ' + request.path[1:-1].replace(u'/', u' » ').title()
+    if request.path == '/':
+        return ' » %s' % 'Home'
+    return ' » %s' % request.path[1:-1].replace('/', ' » ').title()
 
 #234567891123456789212345678931234567894123456789512345678961234567897123456789
